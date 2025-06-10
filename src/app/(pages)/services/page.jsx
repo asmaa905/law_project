592b7a807e1c4e9c21465f9b8519dc7d2fab8699
asmaa905 <git>
2025-06-10 23:12:59 +0300

    handle when press on any service it show coresponding contentand make it active in services page

--- src/app/(pages)/services/page.jsx
+++ src/app/(pages)/services/page.jsx
@@ -10,6 +10,24 @@ import { useState } from "react";
 
 import * as React from "react";
 export default function Services() {
+    const services = [
+        { id: 1, name: "تأسيس الشركات", title: "تأسيس الشركات", image: "assets/images/business-jpg-74161145593532097-600x338.webp", body: "لدى شركة رواد الحكمة خبرة طويلة في مجال تأسيس الشركات التجارية المحلية والمختلطة بمختلف أنواعها ( المساهمة – المحدودة – التضامنية – التوصية البسيطة – القابضة... الخ ) وما يطرأ عليها من تعديلات وتصفية الشركات - تسجيل العلامات التجارية – تسجيل الوكالات التجارية – استخراج السجلات التجارية والقيام بتعديلاتها – تحويل المؤسسات الفردية إلي شركات – إعداد اللوائح العمالية الداخلية والمهام الوظيفية للشركات والمؤسسات – وضع الأنظمة والمواثيق التي تحافظ على حماية واستقرار الشركات العائلية ( من الأزمات ) – تولي مهام التحكيم في المنازعات التجارية – استصدار تراخيص وسجلات الشركات الأجنبية والمختلطة من الهيئة العامة للاستثمار" },
+        { id: 2, name: "صياغة وتدقيق كافة العقود ", title: "صياغة وتدقيق  كافة العقود", image: "assets/images/aquod.webp", body: "العقود من أهم مصادر الالتزام في التعاملات اليومية بين أفراد المجتمع فيما بينهم أو بين أفراد المجتمع ومؤسساته العامة أو الخاصة أو بين مؤسسات المجتمع العامة والخاصة فيما بينها، وحيث إن الفرد منا في حياته اليومية يبرم العديد من العقود (دون وعي منه بأهميتها والآثار المترتبة عليها) لذا ينبغي عليه المعرفة والعلم بشروط العقود التي أبرمها والالتزامات المترتبة عليها..." },
+        { id: 3, name: "الاستشارات القانونية والشرعية", title: "الاستشارات  القانونية والشرعية", image: "assets/images/consult.webp", body: "تقدم شركة رواد الحكمة كافة الاستشارات القانونية والشرعية الشفوية والمكتوبة فالاستشارة القانونية هي استكشاف رأي النظام و الشرع في مسألة معينة أو موضوع بعينه قد تكون محل نزاع جدي أمام القضاء أو نزاع ممكن أن يقع مستقبلاً..." },
+        { id: 4, name: "التمثيل القانوني", title: "التمثيل القانوني", image: "assets/images/some_service.webp", body: "تتولى شركة رواد الحكمة  المرافعة والمدافعة عن العملاء وتمثيلهم لدي كافة المحاكم الشرعية – ديوان المظالم – لجنة تسوية المنازعات المصرفية – لجان مكتب العمل – لجان مكتب الفصل في منازعات الأوراق التجارية – اللجان شبة القضائية والإدارية – اللجان الجمركية – المنازعات أمام هيئة التحكيم المحلية والدولية" },
+        { id: 5, name: "التجارية", title: "قضايا  التجارية وتحصيل الديون", image: "assets/images/eccomerce.webp", body: "مع صدور نظام التنفيذ بالمملكة العربية السعودية خصصت شركة رواد الحكمة قسم مستقل لقضايا الأوراق التجارية ( السند لأمر – الكمبيالة – الشيك ) وتحصيل الديون يضم نخبة من أفضل المحاميين والمستشارين في قضايا الأوراق التجارية." },
+        { id: 6, name: "الجنائية", title: "قضايا  الجنائية", image: "assets/images/genaay.webp", body: "- الجرائم المعلوماتية. - قضايا المخدرات ( التعاطي والاتجار- التهريب ) . - قضايا القتل العمد وشبه العمد والشروع بالقتل. - قضايا السرقات . - قضايا الخطف. - قضايا الحرابة..." },
+        { id: 7, name: "العمالية والادارية", title: "قضايا  العماليةوالإدارية", image: "assets/images/workers.webp", body: "تعد شركة رواد الحكمة من أمهر وأفضل المحامين بالمملكة العربية السعودية بقضايا الشركات فقد قام بالعديد من الأبحاث المتعلقة بعمل الشركات ومنها ( حماية شركات المساهمة من الإفلاس ، وحماية الشركات العائلية من الأزمات )..." },
+        { id: 8, name: "الأحوال الشخصية", title: "قضايا الأحوال الشخصية", image: "assets/images/personal_statuses.webp", body: "تعد قضايا الأحوال الشخصية والأسرية وما تتضمنه من:- - قضايا الطلاق والخلع وفسخ النكاح للضرر. - قضايا النفقة . - قضايا الحضانة والزيارة. - قضايا النشوز . - قضايا إثبات الوقف والوصية والنسب والغيبة والوفاة وحصر الورثة وقسمة التركة..." },
+    ];
+
+    // State to track the active service
+    const [activeService, setActiveService] = useState(services[0]);
+
+    // Function to handle menu item click
+    const handleServiceClick = (service) => {
+        setActiveService(service);
+    };
     return (
         <>
             <div className="services my-44">
@@ -272,70 +290,45 @@ export default function Services() {
                             </button>
                             <h2 className=" font-[700] text-[#134776] text-[38px] leading-539px] text-center sm:text-right " >خدماتنا بعمق</h2>
                         </div>
-                        <div className="body flex flex-col-reverse lg:flex-row border-2 flex-wrap
-                        border-[#ccc] justify-between items-start  sm:px-0 px-3 sm:pb-0 pb-3">
-                            <div className="sm:pt-0 w-full lg:w-2/3 flex flex-col-reverse flex-wrap  md:flex-row justify-end items-center pt-[50px] mt-[52px] sm:px-5 px-0">
-                                <div className="image w-full lg:w-1/3">
-                                    <img src="assets/images/business-jpg-74161145593532097-600x338.webp" alt="some_service" className="w-full h-full" />
+                        <div className="body flex flex-col-reverse md:flex-row border-2 border-[#ccc] justify-between items-start mt-5  sm:px-0 px-3 sm:pb-0 pb-3">
+                            <div className="pt-[28px] sm:pt-0 w-full sm:w-2/3 flex flex-col-reverse  md:flex-row justify-end items-start sm:px-5 px-0">
+                                <div className="image w-full  sm:w-1/3">
+                                    <img src={activeService.image} alt={activeService.title} className="w-full h-full" />
                                 </div>
-                                <div className="text w-full lg:w-2/3 text-right ">
+                                <div className="text w-full sm:w-2/3 text-right ">
                                     <h3 className="font-[700] text-[rgb(19,_71,_118)] text-[32px] leading-[45px] pb-5">
-                                        <span className=" text-[rgb(19,_71,_118)] ">تأسيس</span>
-                                        &nbsp; <span className=" text-[#C9A85E] ">الشركات</span>
+                                        <span className=" text-[rgb(19,_71,_118)] ">{activeService.title.split(' ')[0]}</span>
+                                        &nbsp; <span className=" text-[#C9A85E] ">{activeService.title.split(' ').slice(1).join(' ')}</span>
                                     </h3>
-                                    <p className="paragaph font-[300] text-[rgb(68,_68,_68)] text-[18px] leading-[29px] ">لدى شركة رواد الحكمة خبره طويلة في مجال تأسيس الشركات التجارية المحلية والمختلطة بمختلف أنواعها ( المساهمة – المحدودة – التضامنية – التوصية البسيطة – القابضة... الخ ) وما يطرأ عليها من تعديلات وتصفية الشركات - تسجيل العلامات التجارية – تسجيل الوكالات التجارية – استخراج السجلات التجارية والقيام بتعديلاتها – تحويل المؤسسات الفردية إلي شركات – إعداد اللوائح العمالية الداخلية والمهام الوظيفية للشركات والمؤسسات – وضع الأنظمة التي تحافظ على حماية واستقرار الشركات العائلية ( من الأزمات ) – تولي مهام التحكيم في المنازعات التجارية – استصدار تراخيص وسجلات الشركات الأجنبية والمختلطة من الهيئة العامة للاستثمار
-
+                                    <p className="paragaph font-[300] text-[rgb(68,_68,_68)] text-[18px] leading-[29px] ">
+                                        {activeService.body}
                                     </p>
                                 </div>
                             </div>
-
-
-                            <div className="w-full md:w-1/3 bg-[#134776]">
+                            <div className="w-full sm:w-1/3 bg-[#134776]">
                                 <ul className="w-full h-full">
-                                    <li className="p-[12px_5px] sm:p-[12px_20px] hover:bg-white text-[16px] leading-[26px] font-[400]  
-                                                cursor-pointer hover:text-[rgb(19,_71,_118)] text-white bg-[rgb(19,_71,_118)]
-                                                 flex flex-row gap-2 justify-end items-center  active">
-                                        {/*  onClick={openServiceDetails('تأسيس الشركات')} */}
-                                        <span className="">تأسيس الشركات</span>
-                                        <FontAwesomeIcon icon={faBusinessTime} />
-                                    </li>
-
-
-                                    <li className="p-[12px_5px] sm:p-[12px_20px] hover:bg-white text-[16px] leading-[26px] font-[400]  
-                                                cursor-pointer hover:text-[rgb(19,_71,_118)] text-white bg-[rgb(19,_71,_118)]
-                                                 flex flex-row gap-2 justify-end items-center   text-right ">                                   <span className="">صياغة وتدقيق كافة العقود </span>
-                                        <FontAwesomeIcon icon={faFileContract} />
-                                    </li>
-                                    <li className="p-[12px_5px] sm:p-[12px_20px] hover:bg-white text-[16px] leading-[26px] font-[400]  
-                                                cursor-pointer hover:text-[rgb(19,_71,_118)] text-white bg-[rgb(19,_71,_118)]
-                                                 flex flex-row gap-2 justify-end items-center  text-right ">                                       <span className="">الاستشارات القانونية والشرعية</span>
-                                        <FontAwesomeIcon icon={faBalanceScale} />
-                                    </li>
-                                    <li className="p-[12px_5px] sm:p-[12px_20px] hover:bg-white text-[16px] leading-[26px] font-[400]  
-                                                cursor-pointer hover:text-[rgb(19,_71,_118)] text-white bg-[rgb(19,_71,_118)]
-                                                 flex flex-row gap-2 justify-end items-center  text-right ">                                      <span className="">التمثيل القانوني</span>
-                                        <FontAwesomeIcon icon={faGavel} />
-                                    </li>
-                                    <li className="p-[12px_5px] sm:p-[12px_20px] hover:bg-white text-[16px] leading-[26px] font-[400]  
-                                                cursor-pointer hover:text-[rgb(19,_71,_118)] text-white bg-[rgb(19,_71,_118)]
-                                                 flex flex-row gap-2 justify-end items-center  text-right ">                                       <span className="">التجارية</span>
-                                        <FontAwesomeIcon icon={faBuilding} />
-                                    </li>
-                                    <li className="p-[12px_5px] sm:p-[12px_20px] hover:bg-white text-[16px] leading-[26px] font-[400]  
-                                                cursor-pointer hover:text-[rgb(19,_71,_118)] text-white bg-[rgb(19,_71,_118)]
-                                                 flex flex-row gap-2 justify-end items-center text-right ">                                       <span className="">الجنائية</span>
-                                        <FontAwesomeIcon icon={faMask} />
-                                    </li>
-                                    <li className="p-[12px_5px] sm:p-[12px_20px] hover:bg-white text-[16px] leading-[26px] font-[400]  
-                                                cursor-pointer hover:text-[rgb(19,_71,_118)] text-white bg-[rgb(19,_71,_118)]
-                                                 flex flex-row gap-2 justify-end items-center text-right ">                                      <span className=""> العمالية والادارية</span>
-                                        <FontAwesomeIcon icon={faUserPlus} />
-                                    </li>
-                                    <li className="p-[12px_5px] sm:p-[12px_20px] hover:bg-white text-[16px] leading-[26px] font-[400]  
-                                                cursor-pointer hover:text-[rgb(19,_71,_118)] text-white bg-[rgb(19,_71,_118)]
-                                                 flex flex-row gap-2 justify-end items-center text-right ">                                       <span className="">الأحوال الشخصية</span>
-                                        <FontAwesomeIcon icon={faUsers} />
-                                    </li>
+                                    {services.map((service) => (
+                                        <li
+                                            key={service.id}
+                                            className={`p-[12px_5px] sm:p-[12px_20px] hover:bg-white text-[16px] leading-[26px] font-[400]  
+                                                               cursor-pointer hover:text-[rgb(19,_71,_118)] 
+                                                               flex flex-row gap-2 justify-end items-center text-right 
+                                                               ${activeService.id === service.id ? 'bg-white text-[rgb(19,_71,_118)]' : 'bg-[rgb(19,_71,_118)] text-white'}`}
+                                            onClick={() => handleServiceClick(service)}
+                                        >
+                                            <span>{service.name}</span>
+                                            <FontAwesomeIcon icon={
+                                                service.id === 1 ? faBusinessTime :
+                                                    service.id === 2 ? faFileContract :
+                                                        service.id === 3 ? faBalanceScale :
+                                                            service.id === 4 ? faGavel :
+                                                                service.id === 5 ? faBuilding :
+                                                                    service.id === 6 ? faMask :
+                                                                        service.id === 7 ? faUserPlus :
+                                                                            faUsers
+                                            } />
+                                        </li>
+                                    ))}
                                 </ul>
                             </div>
                         </div>
